9b87bad3c702ebf4c8f5cec52942d52f50b43efc
JoshCrz <35774174+JoshCrz@users.noreply.github.com>
2020-07-29 21:52:51 +0100

    added model state and endpoints

--- CityInfo/CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo/CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,7 +19,7 @@ namespace CityInfo.API.Controllers
             var city = CitiesDataStore.Current.Cities
                 .FirstOrDefault(x => x.Id == cityId);
 
-            if(city == null)
+            if (city == null)
             {
                 return NotFound();
             }
@@ -25,7 +28,8 @@ namespace CityInfo.API.Controllers
 
         }
 
-        [HttpGet("{id}")]
+        //by giving the endpoint a name, we can refer to it when returning from out POST
+        [HttpGet("{id}", Name = "GetPointOfInterest")]
         public IActionResult GetPointOfInterest(int cityId, int id)
         {
             var city = CitiesDataStore.Current.Cities
@@ -39,7 +43,7 @@ namespace CityInfo.API.Controllers
             var pointOfInterest = city.PointsOfInterest
                 .FirstOrDefault(c => c.Id == id);
 
-            if(pointOfInterest == null)
+            if (pointOfInterest == null)
             {
                 return NotFound();
             }
@@ -47,6 +51,86 @@ namespace CityInfo.API.Controllers
             return Ok(pointOfInterest);
         }
 
+        [HttpPost]
+        public IActionResult CreatePointOfInterest(int cityId,
+            [FromBody] PointOfInterestForCreationDto pointOfInterest)
+        {
+            if (pointOfInterest.Description == pointOfInterest.Name)
+            {
+                ModelState.AddModelError(
+                    "Description",
+                    "The provided description should be different from the name.");
+            }
+
+            //check the model validation - this is automatically happenign as we use the API controller
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            //demo purposes, this will be improved
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(
+                c => c.PointsOfInterest).Max(p => p.Id);
+
+            var finalPointOfInterest = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+
+            city.PointsOfInterest.Add(finalPointOfInterest);
+
+            return CreatedAtRoute(
+                "GetPointOfInterest",
+                new { cityId, id = finalPointOfInterest.Id },
+                finalPointOfInterest);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdatePointOfInterest(int cityId, int id, 
+            [FromBody] PointOfInterestForUpdateDto pointOfInterest)
+        {
+
+            if (pointOfInterest.Description == pointOfInterest.Name)
+            {
+                ModelState.AddModelError(
+                    "Description",
+                    "The provided description should be different from the name.");
+            }
+
+            //check the model validation - this is automatically happenign as we use the API controller
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if(city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = city.PointsOfInterest.FirstOrDefault(x => x.Id == id);
+
+            if(pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            pointOfInterestFromStore.Name = pointOfInterest.Name;
+            pointOfInterestFromStore.Description = pointOfInterest.Description;
+
+            //NoContent return type means the request completed successfully and there's nothing to return;
+            return NoContent(); 
+
+        }
 
     }
 }
